25c342f9734d9be30fc6eae7f1591bb798c775c2
Sayandeep <sayandeep11@gmail.com>
2023-08-10 07:57:42 +0530

    Grant and Revoke details access

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
 app.get("/preg", (req, res) => {
     res.sendFile(path.join(__dirname + "/patientregister.html"));
 })
+app.get("/givepermi", (req, res) => {
+    res.sendFile(path.join(__dirname + "/givepermi.html"));
+})
+app.get("/revokepermi", (req, res) => {
+    res.sendFile(path.join(__dirname + "/revokepermi.html"));
+})
 const server = app.listen(2000);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
